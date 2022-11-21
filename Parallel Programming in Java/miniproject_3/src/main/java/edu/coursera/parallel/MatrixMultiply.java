b9ef951e18d6aa43659a4f195d8aaa65979b5692
Андрей Тарасенко <mikatohimemiya@gmail.com>
2022-11-21 22:28:09 +0200

    Week #3 miniproject #3 solution

--- Parallel Programming in Java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
+++ Parallel Programming in Java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -1,6 +1,6 @@
 package edu.coursera.parallel;
 
-import static edu.rice.pcdp.PCDP.forseq2d;
+import static edu.rice.pcdp.PCDP.*;
 
 /**
  * Wrapper class for implementing matrix multiply efficiently in parallel.
@@ -21,7 +21,7 @@ public final class MatrixMultiply {
      * @param N Size of each dimension of the input matrices
      */
     public static void seqMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
+                                         final double[][] C, final int N) {
         forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
@@ -39,12 +39,12 @@ public final class MatrixMultiply {
      * @param N Size of each dimension of the input matrices
      */
     public static void parMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
+                                         final double[][] C, final int N) {
         /*
          * TODO Parallelize this outermost two-dimension sequential loop to
          * achieve performance improvement.
          */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+        forall2dChunked(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
