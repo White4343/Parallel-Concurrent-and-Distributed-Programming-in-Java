541c80ab1d064974b840919ccb3ab60335574e3d
Андрей Тарасенко <mikatohimemiya@gmail.com>
2022-11-22 22:27:39 +0200

    Course #2 Week #3 miniproject #3 solution

--- Concurrent Programming in Java/miniproject_3/src/main/java/edu/coursera/concurrent/SieveActor.java
+++ Concurrent Programming in Java/miniproject_3/src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -2,16 +2,21 @@ package edu.coursera.concurrent;
 
 import edu.rice.pcdp.Actor;
 
+import static edu.rice.pcdp.PCDP.finish;
+
+import java.util.stream.IntStream;
+
+
 /**
  * An actor-based implementation of the Sieve of Eratosthenes.
- *
+ * <p>
  * TODO Fill in the empty SieveActorActor actor class below and use it from
- * countPrimes to determin the number of primes <= limit.
+ * countPrimes to determine the number of primes <= limit.
  */
 public final class SieveActor extends Sieve {
     /**
      * {@inheritDoc}
-     *
+     * <p>
      * TODO Use the SieveActorActor class to calculate the number of primes <=
      * limit in parallel. You might consider how you can model the Sieve of
      * Eratosthenes as a pipeline of actors, each corresponding to a single
@@ -19,7 +24,20 @@ public final class SieveActor extends Sieve {
      */
     @Override
     public int countPrimes(final int limit) {
-        throw new UnsupportedOperationException();
+        SieveActorActor[] sieveActor = new SieveActorActor[1];
+        finish(() -> {
+            sieveActor[0] = new SieveActorActor(3);
+            IntStream.range(4, limit + 1)
+                    .filter(i -> i % 2 == 1)
+                    .forEach(sieveActor[0]::send);
+        });
+        SieveActorActor loopActor = sieveActor[0];
+        int numPrimes = 1;
+        while (loopActor != null) {
+            loopActor = loopActor.nextActor;
+            numPrimes++;
+        }
+        return numPrimes;
     }
 
     /**
@@ -27,16 +45,29 @@ public final class SieveActor extends Sieve {
      * parallel.
      */
     public static final class SieveActorActor extends Actor {
+        private int prime;
+        private SieveActorActor nextActor;
+
+        SieveActorActor(final int prime) {
+            this.prime = prime;
+        }
+
         /**
          * Process a single message sent to this actor.
-         *
+         * <p>
          * TODO complete this method.
          *
          * @param msg Received message
          */
         @Override
         public void process(final Object msg) {
-            throw new UnsupportedOperationException();
+            int candidate = (Integer) msg;
+            if (candidate % prime != 0) {
+                if (nextActor == null)
+                    nextActor = new SieveActorActor(candidate);
+                else
+                    nextActor.send(msg);
+            }
         }
     }
 }
